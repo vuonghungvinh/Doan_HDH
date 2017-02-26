e1b5323f65753ec2aa9b51df37240b93d66ddceb
Vuong Hung Vinh <vuonghungvinhit@gmail.com>
2017-02-27 00:17:19 +0700

    update

---
+++ FileExploer/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace FileExploer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeDriver = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lvcontainer = new System.Windows.Forms.ListView();
+            this.imglistview = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeDriver
+            // 
+            this.treeDriver.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeDriver.ImageIndex = 0;
+            this.treeDriver.ImageList = this.imageList1;
+            this.treeDriver.Location = new System.Drawing.Point(0, 0);
+            this.treeDriver.Name = "treeDriver";
+            this.treeDriver.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.treeDriver.SelectedImageIndex = 0;
+            this.treeDriver.Size = new System.Drawing.Size(211, 356);
+            this.treeDriver.TabIndex = 2;
+            this.treeDriver.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeDriver_BeforeExpand);
+            this.treeDriver.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeDriver_AfterSelect);
+            this.treeDriver.Click += new System.EventHandler(this.treeDriver_Click);
+            this.treeDriver.MouseClick += new System.Windows.Forms.MouseEventHandler(this.treeDriver_MouseClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "cd_drive.png");
+            this.imageList1.Images.SetKeyName(1, "device cd empty.png");
+            this.imageList1.Images.SetKeyName(2, "drive disk.png");
+            this.imageList1.Images.SetKeyName(3, "drive error.png");
+            this.imageList1.Images.SetKeyName(4, "drive link.png");
+            this.imageList1.Images.SetKeyName(5, "drive locked.png");
+            this.imageList1.Images.SetKeyName(6, "drive network.png");
+            this.imageList1.Images.SetKeyName(7, "drive.png");
+            this.imageList1.Images.SetKeyName(8, "folder.png");
+            this.imageList1.Images.SetKeyName(9, "folder_open.png");
+            this.imageList1.Images.SetKeyName(10, "mycomputer.png");
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeDriver);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lvcontainer);
+            this.splitContainer1.Size = new System.Drawing.Size(635, 356);
+            this.splitContainer1.SplitterDistance = 211;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // lvcontainer
+            // 
+            this.lvcontainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvcontainer.LargeImageList = this.imglistview;
+            this.lvcontainer.Location = new System.Drawing.Point(0, 0);
+            this.lvcontainer.Name = "lvcontainer";
+            this.lvcontainer.ShowItemToolTips = true;
+            this.lvcontainer.Size = new System.Drawing.Size(420, 356);
+            this.lvcontainer.TabIndex = 0;
+            this.lvcontainer.UseCompatibleStateImageBehavior = false;
+            this.lvcontainer.DoubleClick += new System.EventHandler(this.lvcontainer_DoubleClick);
+            // 
+            // imglistview
+            // 
+            this.imglistview.ColorDepth = System.Windows.Forms.ColorDepth.Depth16Bit;
+            this.imglistview.ImageSize = new System.Drawing.Size(48, 48);
+            this.imglistview.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 356);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "FileExploer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeDriver;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView lvcontainer;
+        private System.Windows.Forms.ImageList imglistview;
+    }
+}
+
---
+++ FileExploer/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileExploer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //get a list of the drives
+            string[] drives = Environment.GetLogicalDrives();
+            TreeNode mp = new TreeNode("My Computer", 10, 10);
+            mp.Tag = "con";
+            foreach (string drive in drives)
+            {
+                DriveInfo di = new DriveInfo(drive);
+                int driveImage;
+
+                switch (di.DriveType)    //set the drive's icon
+                {
+                    case DriveType.CDRom:
+                        driveImage = 0;
+                        break;
+                    case DriveType.Network:
+                        driveImage = 6;
+                        break;
+                    case DriveType.NoRootDirectory:
+                        driveImage = 3;
+                        break;
+                    case DriveType.Unknown:
+                        driveImage = 3;
+                        break;
+                    default:
+                        driveImage = 7;
+                        break;
+                }
+
+                TreeNode node = new TreeNode(drive.Substring(0, 1), driveImage, driveImage);
+                node.Tag = drive;
+
+                if (di.IsReady == true)
+                    node.Nodes.Add("...");
+                mp.Nodes.Add(node);
+            }
+            treeDriver.Nodes.Add(mp);
+        }
+
+        private void treeDriver_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void treeDriver_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            if (e.Node.Nodes.Count > 0)
+            {
+                if (e.Node.Nodes[0].Text == "..." && e.Node.Nodes[0].Tag == null)
+                {
+                    e.Node.Nodes.Clear();
+
+                    //get the list of sub direcotires
+                    string[] dirs = Directory.GetDirectories(e.Node.Tag.ToString());
+
+                    foreach (string dir in dirs)
+                    {
+                        DirectoryInfo di = new DirectoryInfo(dir);
+                        TreeNode node = new TreeNode(di.Name, 8, 9);
+
+                        try
+                        {
+                            node.Tag = dir;  //keep the directory's full path in the tag for use later
+
+                            //if the directory has any sub directories add the place holder
+                            if (di.GetDirectories().Count() > 0)
+                                node.Nodes.Add(null, "...", 8, 8);
+                        }
+                        catch (UnauthorizedAccessException) 
+                        { 
+                            //if an unauthorized access exception occured display a locked folder
+                            node.ImageIndex = 5;
+                            node.SelectedImageIndex = 5;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "DirectoryLister", MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                        }
+                        finally
+                        {
+                            e.Node.Nodes.Add(node);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private String processsize(long size)
+        {
+            if (size/(1024.0 * 1024 * 1024)>1)
+            {
+                Decimal rs = size/(Decimal)(1024.0 * 1024 * 1024);
+                rs = Math.Round(rs, 2);
+                return rs.ToString() + " GB";
+            }
+            else if (size / (1024 * 1024) > 1)
+            {
+                Decimal rs = size / (Decimal)(1024 * 1024);
+                rs = Math.Round(rs, 2);
+                return rs.ToString() + " MB";
+            } else if (size / (1024.0 ) > 1)
+            {
+                Decimal rs = size / (Decimal)(1024);
+                rs = Math.Round(rs, 2);
+                return rs.ToString() + " Kb";
+            }
+            else
+            {
+                return size.ToString() + " byte";
+            }
+        }
+        private void showListFile(string path)
+        {
+            int k = 0;
+            string fi;
+            imglistview.Images.Clear();
+            lvcontainer.Clear();
+            try
+            {
+                foreach (string d in Directory.GetDirectories(path))
+                {
+                    imglistview.Images.Add(k.ToString(), SpyIcon.GetIcon(d));
+                    fi = d.Substring(Directory.GetParent(d).ToString().Length + 1);//Lấy tên file
+                    ListViewItem item = new ListViewItem(fi, k);//Tạo item mới
+                    FileInfo finfo = new FileInfo(d);
+                    try
+                    {
+                        item.ToolTipText = "created: " + finfo.CreationTime.ToString() + "\n Size: " + processsize(finfo.Length);
+                    }
+                    catch (Exception ex)
+                    {
+                        item.ToolTipText = "created: " + finfo.CreationTime.ToString() + "\n Size: None";
+                    }
+                    item.Tag = d;//Đánh dấu đường dẫn file cho item
+                    lvcontainer.Items.Add(item);//Thêm item vào listview
+                    k++;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            try
+            {
+                foreach (string f in Directory.GetFiles(path))
+                {
+                    Icon ic = SpyIcon.GetIcon(f);
+                    if (ic != null)
+                    {
+                        imglistview.Images.Add(k.ToString(), ic);
+                    }
+                    else
+                    {
+                        imglistview.Images.Add(k.ToString(), Icon.FromHandle(((Bitmap)imageList1.Images[0]).GetHicon()));
+                    }
+                    FileInfo finfo = new FileInfo(f);
+                    fi = f.Substring(Directory.GetParent(f).ToString().Length + 1);//Lấy tên file
+                    ListViewItem item = new ListViewItem(fi, k);//Tạo item mới
+                    try
+                    {
+                        item.ToolTipText = "created: " + finfo.CreationTime.ToString() + "\n Size: " + processsize(finfo.Length);
+                    }
+                    catch (Exception ex)
+                    {
+                        item.ToolTipText = "created: " + finfo.CreationTime.ToString() + "\n Size: None";
+                    }
+                    //item.ToolTipText = "created: " + finfo.CreationTime.ToString() + "\n Size: " + (di.TotalSize.ToString());
+                    item.Tag = f;//Đánh dấu đường dẫn file cho item
+                    lvcontainer.Items.Add(item);//Thêm item vào listview
+                    k++;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private void treeDriver_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lvcontainer_DoubleClick(object sender, EventArgs e)
+        {
+            FileAttributes attr = File.GetAttributes(lvcontainer.SelectedItems[0].Tag.ToString());
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                showListFile(lvcontainer.SelectedItems[0].Tag.ToString());
+            }
+            else
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start(lvcontainer.SelectedItems[0].Tag.ToString());
+                } catch(Exception ex)
+                {
+                    MessageBox.Show("Can't open file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    System.Diagnostics.Process.Start(new FileInfo(lvcontainer.SelectedItems[0].Tag.ToString()).Directory.FullName);
+                }
+            }
+        }
+
+        private void treeDriver_MouseClick(object sender, MouseEventArgs e)
+        {
+            var hitTest = treeDriver.HitTest(e.Location);
+            if (hitTest.Location != TreeViewHitTestLocations.PlusMinus)
+            {
+                TreeViewHitTestInfo info = treeDriver.HitTest(treeDriver.PointToClient(Cursor.Position));
+                if (info != null && info.Node.Tag != "con")
+                {
+                    showListFile(info.Node.Tag.ToString());
+                }
+            }
+        }
+    }
+}
